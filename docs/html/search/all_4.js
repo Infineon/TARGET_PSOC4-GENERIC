c75677cf688a06ec6c863b45b787dfe020ca92ed
gitlab-runner <do-not-reply@cypress.com>
2022-10-06 02:58:50 -0500

    Upload TARGET_PSOC4-GENERIC 3.0.0.29660

--- docs/html/search/all_4.js
+++ docs/html/search/all_4.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['pin_20states',['Pin States',['../group__group__bsp__pin__state.html',1,'']]],
-  ['pin_20mappings',['Pin Mappings',['../group__group__bsp__pins.html',1,'']]],
-  ['psoc4_2dgeneric_20bsp',['PSOC4-GENERIC BSP',['../index.html',1,'']]],
-  ['peripheral_20default_20hal_20settings',['Peripheral Default HAL Settings',['../md_source_bsps_psoc4_PSOC4-GENERIC_bsp_settings.html',1,'']]]
+  ['peripheral_20default_20hal_20settings_0',['Peripheral Default HAL Settings',['../md_source_bsps_cat2_PSOC4_GENERIC_bsp_settings.html',1,'']]],
+  ['pin_20mappings_1',['Pin Mappings',['../group__group__bsp__pins.html',1,'']]],
+  ['pin_20states_2',['Pin States',['../group__group__bsp__pin__state.html',1,'']]],
+  ['psoc4_2dgeneric_20bsp_3',['PSOC4-GENERIC BSP',['../index.html',1,'']]]
 ];
